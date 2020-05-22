2a968ef39d8915798408f6f2dc4a320fc939fb52
himanshu8496 <eeehimanshu22@gmail.com>
2020-05-22 12:34:15 +0530

    Updates

---
+++ precache-manifest.ddf3a7e96073b6caced791070f1230bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797de21755bdbd25ea4cc54973992dd3",
+    "url": "/himanshu849/my-new-app.git/index.html"
+  },
+  {
+    "revision": "3a4708b046a276ef6dd4",
+    "url": "/himanshu849/my-new-app.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "961cd3b6452dbd64acaf",
+    "url": "/himanshu849/my-new-app.git/static/js/2.4113189c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/himanshu849/my-new-app.git/static/js/2.4113189c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4708b046a276ef6dd4",
+    "url": "/himanshu849/my-new-app.git/static/js/main.9d680af6.chunk.js"
+  },
+  {
+    "revision": "5ac93a729263c478d33c",
+    "url": "/himanshu849/my-new-app.git/static/js/runtime-main.8da430e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/himanshu849/my-new-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
